a18972cb0a7da06af2438a5ee60fde1c66dce568
Joshua Ling <joshling1919@gmail.com>
2018-06-27 12:27:02 -0700

    Test staging

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Greeting = ({ currentUser, logout }) => {
   const sessionLinks = () => (
     <nav className="login-signup">
-      <Link to="/login">Login</Link>
+      <Link to="/login">COME ON IN</Link>
       &nbsp;or&nbsp;
       <Link to="/signup">Sign up!</Link>
     </nav>
