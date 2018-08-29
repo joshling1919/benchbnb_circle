2310fa9621d8679db8a305dfcbfd8466847043c4
Joshua Ling <joshling1919@gmail.com>
2018-08-29 12:46:49 -0700

    Add yay

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import {
-  Route,
-  Redirect,
-  Switch,
-  Link,
-  HashRouter
-} from 'react-router-dom';
+import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
 
 import GreetingContainer from './greeting/greeting_container';
 import SignUpFormContainer from './session_form/signup_form_container';
@@ -20,14 +14,18 @@ const App = () => (
   <div>
     <header>
       <Link to="/" className="header-link">
-        <h1>Bench BnB</h1>
+        <h1>Bench BnB yay</h1>
       </Link>
       <GreetingContainer />
     </header>
     <Switch>
       <AuthRoute exact path="/login" component={LogInFormContainer} />
       <AuthRoute exact path="/signup" component={SignUpFormContainer} />
-      <ProtectedRoute exact path="/benches/new" component={BenchFormContainer} />
+      <ProtectedRoute
+        exact
+        path="/benches/new"
+        component={BenchFormContainer}
+      />
       <Route path="/benches/:benchId" component={BenchShowContainer} />
       <Route exact path="/" component={SearchContainer} />
     </Switch>
